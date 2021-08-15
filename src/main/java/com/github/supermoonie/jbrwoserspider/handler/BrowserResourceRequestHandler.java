53453acc3eff242a29db2df888897446ad3402ab
super_w <wangchao_moonie@163.com>
2021-08-15 23:51:49 +0800

    dev

--- src/main/java/com/github/supermoonie/jbrwoserspider/handler/BrowserResourceRequestHandler.java
+++ src/main/java/com/github/supermoonie/jbrwoserspider/handler/BrowserResourceRequestHandler.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.github.supermoonie.jbrwoserspider.browser.JCefBrowser;
 import com.github.supermoonie.jbrwoserspider.browser.JCefClient;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.cef.browser.CefBrowser;
 import org.cef.browser.CefFrame;
 import org.cef.handler.CefResourceRequestHandlerAdapter;
@@ -26,14 +27,30 @@ public class BrowserResourceRequestHandler extends CefResourceRequestHandlerAdap
 
     @Override
     public boolean onBeforeResourceLoad(CefBrowser browser, CefFrame frame, CefRequest request) {
+        final String url = request.getURL();
+        log.info("url: {}", url);
+        if (url.endsWith(".mp4")) {
+            JCefBrowser jCefBrowser = JCefClient.getInstance().getBrowser(browser);
+            if (null == jCefBrowser) {
+                return true;
+            }
+            Map<String, String> headers = new HashMap<>();
+            request.getHeaderMap(headers);
+            CefBrowser workCefBrowser = jCefBrowser.getWorkCefBrowser();
+            workCefBrowser.executeJavaScript(String.format("window._COMMON.setUrl('%s')", url), null, 0);
+            workCefBrowser.executeJavaScript(String.format("window._COMMON.setFileName('%s')", StringUtils.isEmpty(jCefBrowser.getTitle()) ? "视频" : jCefBrowser.getTitle()), null, 0);
+            workCefBrowser.executeJavaScript(String.format("window._COMMON.setMediaType('%s')", "video/mp4"), null, 0);
+            workCefBrowser.executeJavaScript(String.format("window._COMMON.setHeaders('%s')", JSON.toJSONString(headers)), null, 0);
+        }
         return super.onBeforeResourceLoad(browser, frame, request);
     }
 
     @Override
     public void onResourceLoadComplete(CefBrowser browser, CefFrame frame, CefRequest request, CefResponse response, CefURLRequest.Status status, long receivedContentLength) {
-        String contentType = response.getMimeType();
+        String contentType = response.getMimeType().trim();
         final String url = request.getURL();
-        if (VIDEO_TYPES.contains(contentType)) {
+        log.info("url: {}, contentType: {}", url, contentType);
+        if (VIDEO_TYPES.contains(contentType) && !url.endsWith(".mp4")) {
             JCefBrowser jCefBrowser = JCefClient.getInstance().getBrowser(browser);
             if (null == jCefBrowser) {
                 return;
